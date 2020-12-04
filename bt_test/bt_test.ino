6f8f07eb5172398e4a265ac41b3732508536838f
unknown <everettbrandt@gmail.com>
2020-12-04 14:31:07 -0800

    Testing environment set up

--- bt_test/bt_test.ino
+++ bt_test/bt_test.ino
@@ -8,19 +8,47 @@ SoftwareSerial serialConnection(RXPIN, TXPIN);
 
 char inChar=-1;
 bool autonomous = true;
+bool start = false;
+unsigned int message_size = 0;
 
 void setup(){
   Serial.begin(9600);//Initialize communications to the serial monitor in the Arduino IDE
   serialConnection.begin(9600);//Initialize communications with the bluetooth module
 }
+
 void loop(){
-  checkBluetooth();
-  serialConnection.print("a");
+  if(start == true){
+    bt_write();
+    if(message_size > 15){
+      message_size = 0;
+      start = false;
+    }
+    //Serial.println("Started");
+    //serialConnection.print("hello world");
+  }
+  else{
+    if(serialConnection.available() > 0){
+      inChar = serialConnection.read();//Read one byte
+      if(inChar == ' '){
+        start = true;
+      }
+    }
+  }
 }
 
-void checkBluetooth(){
-  if(serialConnection.available() > 0){
-    inChar = serialConnection.read();//Read one byte
-    Serial.println(inChar);
+void bt_write(){
+  message_size += 1;
+  char message [message_size];
+  for(unsigned int i = 0; i < message_size; i++){
+    if(i%2 == 0){
+      message[i] = '0';
+    }
+    else{
+      message[i] = '1';
+    }
   }
+  message[message_size] = '\0';
+  serialConnection.print(message_size);
+  Serial.print(message_size);
+  Serial.println(message);
 }
