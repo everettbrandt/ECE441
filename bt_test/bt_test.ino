725a857c41e3edb788d5d6ef6647c5fd8f459233
everettbrandt <everettbrandt@gmail.com>
2020-12-04 20:00:46 -0800

    Able to retrieve size

--- bt_test/bt_test.ino
+++ bt_test/bt_test.ino
@@ -4,6 +4,12 @@
 #define RXPIN 2
 #define TXPIN 3
 
+//Maximum size of char array that is sent
+//This is the maximum size of an array I
+//can make without a compiler error (2^15 -1)
+//#define TEST_SIZE 32767
+#define TEST_SIZE 1000
+
 SoftwareSerial serialConnection(RXPIN, TXPIN);
 
 char inChar=-1;
@@ -17,38 +23,47 @@ void setup(){
 }
 
 void loop(){
-  if(start == true){
-    bt_write();
-    if(message_size > 15){
-      message_size = 0;
-      start = false;
-    }
-    //Serial.println("Started");
-    //serialConnection.print("hello world");
-  }
-  else{
-    if(serialConnection.available() > 0){
+  if(serialConnection.available() > 0){
       inChar = serialConnection.read();//Read one byte
-      if(inChar == ' '){
-        start = true;
+      if(inChar == 's'){
+        bt_test();
       }
     }
-  }
 }
 
-void bt_write(){
-  message_size += 1;
-  char message [message_size];
-  for(unsigned int i = 0; i < message_size; i++){
+void bt_test(){
+  char message [TEST_SIZE];
+  unsigned int i;
+  char size_message[6];
+  char temp;
+
+  //Fills the array
+  for(i = 0; i < TEST_SIZE; i++){
     if(i%2 == 0){
-      message[i] = '0';
-    }
-    else{
-      message[i] = '1';
-    }
+        message[i] = '0';
+      }
+      else{
+        message[i] = '1';
+      }
   }
-  message[message_size] = '\0';
-  serialConnection.print(message_size);
-  Serial.print(message_size);
-  Serial.println(message);
+
+  //Sends message of increasing size
+  //places null character at end
+  for(i = 1; i < TEST_SIZE; i++){
+    //set the null character for size
+    temp = message[i];
+    message[i] = '\0';
+
+    //convert the size to a char [] and send it
+    itoa(i, size_message, 10);
+    serialConnection.print(size_message);
+    //Indicates the end of size part
+    serialConnection.print('x');
+
+    //Serial.println(size_message);
+    //Serial.print(j);
+    //Serial.println(message);
+    message[i] = temp;
+  }
+  serialConnection.print('d');
 }
